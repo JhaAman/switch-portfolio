cb2b1a7ec37691325128ae8faf89ea9347a1a336
Aman Jha <gunujha@gmail.com>
2022-03-18 11:32:23 -0700

    skip to content, header displays properly

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import Container from "_/layouts/Container";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <Container
-    // title=""
-    // description=""
-    >
+    <Container>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </Container>
   );
