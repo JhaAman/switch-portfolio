035a955531fecac6272225983c2dd4247e8372ab
Aman Jha <gunujha@gmail.com>
2022-03-18 09:59:48 -0700

    navitem

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,19 +13,6 @@ const Home: NextPage = () => {
       </Head>
 
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
